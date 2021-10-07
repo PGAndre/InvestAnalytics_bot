4ac1298549ed8c1d54834a8e5e1ab3330a90cfd0
PGAndre <apryahin@gmail.com>
2021-10-07 19:37:20 +0300

    users handler

--- tgbot/handlers/user.py
+++ tgbot/handlers/user.py
@@ -4,8 +4,9 @@ from aiogram.types import Message
 
 async def user_start(message: Message):
     user = message.from_user.username
-    await message.reply(f"Hello, {user} !")
+    await message.reply(f"Hello, {user} ! \n /predict, чтобы создать прогноз")
 
 
 def register_user(dp: Dispatcher):
     dp.register_message_handler(user_start, commands=["start"], state="*")
+    dp.register_chat_member_handler()
