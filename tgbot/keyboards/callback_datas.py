1a183014603219b82c1de7a1b4d9c58f2a464739
PGAndre <apryahin@gmail.com>
2022-02-02 04:09:55 +0300

    update bot predicts menu

--- tgbot/keyboards/callback_datas.py
+++ tgbot/keyboards/callback_datas.py
@@ -2,8 +2,8 @@ from aiogram.utils.callback_data import CallbackData
 
 user_callback = CallbackData("user", "action")
 analytic_callback = CallbackData("analytic", "action")
-predict_callback = CallbackData("predict", "ticker")
-user_predict_callback = CallbackData("user_predict", "ticker")
+predict_callback = CallbackData("predict", "ticker", "action")
+user_predict_callback = CallbackData("user_predict", "ticker", "action")
 admin_callback = CallbackData("admin", "action")
 list_analytic_callback = CallbackData("admin", "id", "is_active", "action")
 user_list_analytic_callback = CallbackData("admin", "id", "is_active", "action")
